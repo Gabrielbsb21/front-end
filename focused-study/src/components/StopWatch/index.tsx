97c6b825416423c566b8157fe2da5a53b0fcfbc6
gabrielbsb21 <gabrielbsb21@outlook.com>
2022-10-28 22:40:48 -0300

    ✨ introduce stopwatch style

--- focused-study/src/components/StopWatch/index.tsx
+++ focused-study/src/components/StopWatch/index.tsx
@@ -1,9 +1,17 @@
-import { StopWatchWrapper, Heading } from "./style"
+import { StopWatchWrapper, Heading, Wrapper, Span, Button } from "./style"
 
 export const StopWatch = () => {
   return (
     <StopWatchWrapper>
       <Heading>Escolha uma tarefa e inicie o Cronômetro</Heading>
+      <Wrapper>
+        <Span>0</Span>
+        <Span>0</Span>
+        <Span>:</Span>
+        <Span>0</Span>
+        <Span>0</Span>
+      </Wrapper>
+      <Button>Começar</Button>
     </StopWatchWrapper>
   )
 }
