a974a4b230360ef2233b5124b5598312f8828033
gabrielbsb21 <gabrielbsb21@outlook.com>
2022-11-20 12:13:23 -0300

    🔥 remove code and files

--- focused-study/src/components/Main/style.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const MainWrapper = styled.main`
-  display: grid;
-  grid-template-areas: 'sectionForm asideTask' 'sectionStopWatch .';
-  grid-row-gap: 1rem;
-  grid-column-gap: 4rem;
-`;
