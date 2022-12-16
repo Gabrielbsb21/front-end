9920f35b746d4eb6c52b56654739fa7ea17ca3dd
gabrielbsb21 <gabrielbsb21@outlook.com>
2022-12-16 09:59:14 -0300

    feat: ✨ form

--- organo/src/components/Form/index.jsx
+++ organo/src/components/Form/index.jsx
@@ -1,16 +1,28 @@
 import { WrapperForm, ContributorForm } from './style';
 
 export const Form = () => {
+  const handleForm = (e) => {
+    e.preventDefault();
+  };
   return (
     <WrapperForm>
-      <ContributorForm>
+      <ContributorForm onSubmit={handleForm}>
         <fieldset>
           <legend>Preencha os dados para criar o card do Piloto.</legend>
-          <div>
+          <div className="input-wrapper">
             <label htmlFor="name">Nome</label>
-            <input type="text" id="name" />
+            <input type="text" id="name" placeholder="Digite seu nome" />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="job">Cargo</label>
+            <input type="text" id="job" placeholder="Digite seu cargo" />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="image">Imagem</label>
+            <input type="file" id="image" />
           </div>
         </fieldset>
+        <button type="submit">Criar Card</button>
       </ContributorForm>
     </WrapperForm>
   );
--- organo/src/components/Form/style.js
+++ organo/src/components/Form/style.js
@@ -12,11 +12,43 @@ export const ContributorForm = styled.form`
   border-radius: 20px;
   display: flex;
   justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
   margin-top: 3rem;
   padding: 4rem;
 
   legend {
     color: #212121;
     font-size: 1.75rem;
+  }
+
+  .input-wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    margin-top: 2rem;
+  }
+
+  .input-wrapper label {
+    color: #212121;
+    font-size: 1.25rem;
+  }
+
+  .input-wrapper input {
+    background-color: #ffffff;
+    box-shadow: 10px 10px 30px rgba(0, 0, 0, 0.06);
+    border-radius: 8px;
+    padding: 1rem 0.5rem;
+  }
+
+  button {
+    background-color: #6278f7;
+    border: 1px solid #6278f7;
+    color: #ffffff;
+    cursor: pointer;
+    border-radius: 8px;
+    padding: 0.5rem 1rem;
+    font-size: 1.25rem;
   }
 `;
